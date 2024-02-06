e46e2c65c8a7a7314838384a482dd8fca4c63652
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2024-02-06 23:18:43 +0300

    Home work done

---
+++ main.py
@@ -0,0 +1,39 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+
+    def __get_html_content(self):
+        """ Метод для получения html """
+        with open('index.html', 'r', encoding='utf-8') as file:
+            html_file = file.read()
+        return html_file
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        page_content = self.__get_html_content()
+
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
